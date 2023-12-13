7e6f33fc6fe2efee2577d60738137be4adfc9a0d
Jivitesh Rai <116089872+jivitesh-rai@users.noreply.github.com>
2023-12-13 19:45:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ def find_largest(num1, num2, num3):
   return largest
 
 st.title("Find the Largest Number")
+st.text("By 22f1000475")
 
 num1 = st.number_input("Enter First Number")
 num2 = st.number_input("Enter Second Number")
